edd24d2ffe7be964f274a029082aa7009f49e7d0
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-03-17 16:22:05 -0500

    albums seeder created and albums migrations refactored

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100),
-    name VARCHAR(100),
+    artist VARCHAR(50),
+    name VARCHAR(50),
     release_date INT UNSIGNED,
     sales FLOAT UNSIGNED,
-    genre TEXT,
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
