664ac2f3865b338abd601076c74def3735a7654f
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-03-21 10:01:03 -0500

    completed mysql order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+SELECT *
+FROM employees
+WHERE gender = 'M'
+  AND (
+           first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%e';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND NOT last_name LIKE '%qu%';
